9134f44101711bd7617c2ae77581d29035dcd7bb
unknown <kaungmyatkyaw.hhhh@gmail.com>
2024-10-24 01:52:45 +0800

    Changed index.html and script.js

--- public/script.js
+++ public/script.js
@@ -1,27 +1,29 @@
+const imageContainer = document.getElementById('imageContainer');
+const fileInput = document.getElementById('imageInput');
 
+      
 document.getElementById('uploadForm').addEventListener('submit', async (event) => {
-    console.log(event);
-    event.preventDefault();
+  console.log(event);
+  event.preventDefault();
 
-    const formData = new FormData();
-    const fileInput = document.getElementById('imageInput');
-    // const filenameInput = document.querySelector('input[name="filename"]'); - Deprecated only for handling file names inputted by the user
-    formData.append('image', fileInput.files[0]);
-    // formData.append('filename', filenameInput.value);
+  const formData = new FormData();
+  // const filenameInput = document.querySelector('input[name="filename"]'); - Deprecated only for handling file names inputted by the user
+  formData.append('image', fileInput.files[0]);
+  // formData.append('filename', filenameInput.value);
 
-    const response = await fetch('/upload', {
-      method: 'POST',
-      body: formData,
-    });
-
-    if (response.ok) {
-      fetchImages(); // Refresh the image list after upload
-    } else {
-      console.error('Upload failed:', response.statusText);
-    }
+  const response = await fetch('/upload', {
+  method: 'POST',
+  body: formData,
   });
 
-  async function fetchImages() {
+  if (response.ok) {
+  fetchImages(); // Refresh the image list after upload
+  } else {
+  console.error('Upload failed:', response.statusText);
+  }
+});
+
+async function fetchImages() {
     const response = await fetch('/images');
 
     if (!response.ok) {
@@ -31,10 +33,11 @@ document.getElementById('uploadForm').addEventListener('submit', async (event) =
 
     const imageKeys = await response.json();
 
-    const imageContainer = document.getElementById('imageContainer');
     imageContainer.innerHTML = ''; // Clear existing images
     console.log(imageKeys);
-    imageKeys.forEach(async (key) => {
+
+    let selectedKeys = imageKeys.slice(0, 4);
+    selectedKeys.forEach(async (key) => {
       // Fetch the pre-signed URL for the image
       const urlResponse = await fetch(`/image-url?key=${key}`);
       if (urlResponse.ok) {
@@ -52,8 +55,11 @@ document.getElementById('uploadForm').addEventListener('submit', async (event) =
       } else {
         console.error('Failed to fetch pre-signed URL:', urlResponse.statusText);
       }
+
     });
   }
 
-  // Fetch images when the page loads
-  fetchImages();
+  
+fetchImages()
+
+
