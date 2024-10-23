20ee5b4fa0b7edd00a06928fba9190caf35f37cc
JoWoon Kim <96nicekim@gmail.com>
2024-10-23 18:51:12 +0800

    added server and database connection, uploading meta data

--- db.js
+++ db.js
@@ -1,67 +1,97 @@
 /*
 Project Title : StraySpotter
 Project Description : A web service that utilizes Wasabi cloud, for people to share stray cat pictures.
-                      The data will be analysed to provide the insight of the stray cats.
+  The data will be analysed to provide the insight of the stray cats.
 Member : KIM JOWOON, ..
 Date Started : 21.10.2024
 Current version : 1.0
-Version date : 21.10.2024
+Version date : 23.10.2024
 */
 
-const exifr = require('exifr');
-
-
-// Get the client
-const mysql = require('mysql2');
-
 // Create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'strayspotter_database',
-  password: 'SQL_2410_strayspotter' // Hide the Password!
+const mysql = require('mysql2');
+require('dotenv').config();
+
+// Extracted data from the picture
+let data = {
+  latitude : null,
+  longitude : null,
+  date : null,
+  postcode : 123,
+  district_no : 12,
+  district_name : 'TEST',
+  cat_status : 1,
+};
+
+const exifr = require('exifr');
+let filename = 'resources/sample_image1.jpg';
+extractMetadata(filename).then(metadata => {
+  data.latitude = metadata.latitude;
+  data.longitude = metadata.longitude;
+  data.date = metadata.GPSDateStamp.replaceAll(':','-');
+  insert_data(data);
 });
 
-// A simple SELECT query
-connection.query(
-  'SELECT * FROM pictures WHERE id = 10',
-  function (err, results, fields) {
-    if (err) {
-      console.log(err)
-    }
-    console.log(results[0].id); // results contains rows returned by server
-    // console.log(fields); // fields contains extra meta data about results, if available
-  }
-);
-// Ends the connection
-connection.end();
 
-/// Fuctions////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// Fuctions 
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
- // Function to Extract Meta data
-async function extractMetadata() {
+ // Extract Metadata from the picture
+async function extractMetadata(file) {
     try {
-        // Only GPS metadata
-        let {latitude, longitude} = await exifr.gps('resources/sample_image1.jpg');
-        console.log('GPS Coordinates:', latitude, longitude);
-        
-
-        // Only three specific tags
-        let file = 'resources/sample_image2.HEIC';  // Example file path
-        // let output = await exifr.parse(file, ['ISO', 'Orientation', 'LensModel']);
-        let output = await exifr.parse(file,  ['DateTimeOriginal']);
-        console.log('Selected Tags:', output);
+        // Extract all data
         // let output = await exifr.parse(file, true);
-        
-        file = 'resources/sample_image3.jpg'; 
-        output = await exifr.parse(file,  ['DateTimeOriginal']);
+        let output = await exifr.parse(file, ['GPSLatitude', 'GPSLongitude', 'GPSDateStamp']);
         if (output === undefined) {
-            console.log("Metadata not found or DateTimeOriginal tag is missing.");
+            console.log("Metadata undefined");
         } else {
-            console.log("Metadata:", output.DateTimeOriginal);
+            console.log("Data retrieved!: ");
         }
-        
+        return output;
     } catch (error) {
         console.error('Error reading metadata:', error);
+        return null;
     }
+}
+
+function insert_data(data) {
+  const connection = createDBConnection()
+  // A query to insert data into table
+  connection.query(
+    `INSERT INTO pictures (latitude, longitude, date_taken, postcode, district_no, district_name, cat_status) 
+    VALUES (` + data.latitude + `, ` + data.longitude + `,'` + data.date + `' ,` 
+              + data.postcode + `,` + data.district_no + `, '` + data.district_name + `' ,` 
+              + data.cat_status + `);`
+    ,function (err, results) {
+      if (err) { console.log(err) }
+      console.log(results);
+    }
+  );
+  // Ends the connection
+  connection.end();
+}
+
+function select_data() {
+  const connection = createDBConnection()
+  // A query to insert data into table
+  connection.query(
+    `SELECT * FROM pictures`
+    ,function (err, results) {
+      if (err) { console.log(err) }
+      console.log(results);
+    }
+  );
+  // Ends the connection
+  connection.end();
+}
+
+function createDBConnection() {
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'strayspotter_database',
+    password: process.env.DB_PASSWORD,
+  });
+  return connection;
 }
